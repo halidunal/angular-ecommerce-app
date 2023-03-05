ceacfecbfcb92dc0d39dbd7467f33d4aa29af977
Halid Ünal <halid.unal@oska.com.tr>
2023-03-05 13:58:50 +0300

    routing module configurations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: 'products/:productId', component: ProductComponent},
+  {path: 'products/categories/:categoryId', component: ProductListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
