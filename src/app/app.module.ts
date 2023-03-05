2a47f7c44b472191c894fe245b65cac11aeeed34
Halid Ünal <halid.unal@oska.com.tr>
2023-03-05 14:07:24 +0300

    app component configuration and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductComponent } from './components/product/product.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductListComponent,
+    ProductComponent,
+    CategoryListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
