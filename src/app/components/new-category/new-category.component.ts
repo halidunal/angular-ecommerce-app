5318570423a02e63a0a85e4b1c7c620e707c1804
Halid Ünal <halid.unal@oska.com.tr>
2023-03-15 22:07:02 +0300

    added new-category component

---
+++ src/app/components/new-category/new-category.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CategoryService } from 'src/app/services/category.service';
+
+@Component({
+  selector: 'app-new-category',
+  templateUrl: './new-category.component.html',
+  styleUrls: ['./new-category.component.css'],
+  providers: [CategoryService]
+})
+export class NewCategoryComponent implements OnInit {
+
+  constructor(private categoryService: CategoryService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  createCategory(name: any){
+    this.categoryService.createCategory({name: name.value}).subscribe(result => {
+      this.router.navigate(["/products"])
+    })
+  }
+
+}
