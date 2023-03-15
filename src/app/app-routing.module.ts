93ea822f1457ff6ca7395dde971f441979f26760
Halid Ünal <halid.unal@oska.com.tr>
2023-03-15 22:08:09 +0300

    app component and routing updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { NewCategoryComponent } from './components/new-category/new-category.component';
+import { NewProductComponent } from './components/new-product/new-product.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductComponent } from './components/product/product.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'products', component: ProductListComponent},
+  {path: 'products/new', component: NewProductComponent},
+  {path: 'categories/new', component: NewCategoryComponent},
   {path: 'products/:productId', component: ProductComponent},
   {path: 'products/categories/:categoryId', component: ProductListComponent},
 ];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ProductService]
 })
 export class AppComponent {
-  title = 'ng-app';
+  constructor(private http: HttpClient, private productServices: ProductService) {
+
+  }
+
+
+
 }
